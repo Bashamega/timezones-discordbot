6041c00dc2634b93b90c0032cfaf1ec511a513be
Bashamega <adambashaahmednaji@gmail.com>
2024-09-29 15:01:42 +0300

    feat: new command

--- src/commands/register.js
+++ src/commands/register.js
@@ -1,14 +1,27 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const jsoncommands = require('./data/commands.json');
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const jsoncommands = require("./data/commands.json");
 
 function RegisterCommands(commands) {
-    jsoncommands.map(commandData =>{
-        commands.push(
-          new SlashCommandBuilder()
-            .setName(commandData.name)
-            .setDescription(commandData.description)
-        )
-        console.log(jsoncommands)
-      })
+  jsoncommands.map((commandData) => {
+    const commandBuilder = new SlashCommandBuilder()
+      .setName(commandData.name)
+      .setDescription(commandData.description);
+
+    // Add an option to accept input from users
+    if (commandData.options) {
+      commandData.options.forEach(option => {
+        commandBuilder.addStringOption(optionBuilder =>
+          optionBuilder
+            .setName(option.name)
+            .setDescription(option.description)
+            .setRequired(option.required || false) // Make it required if specified
+        );
+      });
+    }
+
+    commands.push(commandBuilder);
+    console.log(jsoncommands);
+  });
 }
-module.exports = {RegisterCommands}
+
+module.exports = { RegisterCommands };
